3608c836e7f14dcf497a1e8279bac871a656b700
narges-rzv <narges.sharif@gmail.com>
2018-06-01 18:07:50 -0400

    script to get weather data

---
+++ data/data_links.sh
@@ -0,0 +1,9 @@
+#https://www.ncdc.noaa.gov/cdo-web/
+wget https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2018.csv.gz
+wget https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2017.csv.gz
+wget https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2016.csv.gz
+wget https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2015.csv.gz
+wget https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2014.csv.gz
+wget https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2013.csv.gz
+gunzip *.gz
+#manhattan station: USW00094728
